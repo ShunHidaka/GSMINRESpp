a63fca08c30721009755bf2160ed6e18449a1d76
hidaka@HOME <stern.komo@gmail.com>
2025-05-04 05:39:27 +0900

    Fortran interface の実装中

---
+++ sample/sample1_f.f90
@@ -0,0 +1,110 @@
+program sample2_f
+  use gsminres_mod
+  use iso_c_binding
+  implicit none
+
+  ! Parameters
+  integer, parameter :: dp = kind(0.0d0)
+  integer(c_size_t) :: n, m
+  integer :: i, j, info
+  complex(c_double_complex), allocatable :: A(:), B(:), x(:), rhs(:), w(:), u(:), sigma(:), r(:)
+  real(c_double), allocatable :: res(:)
+  integer, allocatable :: itr(:)
+  complex(c_double_complex) :: ONE=cmplx(1.0d0, 0.0d0, kind=c_double_complex), ZERO=cmplx(0.0d0, 0.0d0, kind=c_double_complex)
+
+  ! External BLAS/LAPACK routines
+  external :: zhpmv, zpptrf, zpptrs
+  double precision :: dznrm2
+
+  ! Handle
+  type(gsminres_handle) :: solver
+
+  ! Read matrices A,B form MTX files
+  call load_matrix_from_mm("../data/ELSES_MATRIX_DIAB18h_A.mtx", A, n)
+  call load_matrix_from_mm("../data/ELSES_MATRIX_DIAB18h_B.mtx", B, n)
+
+  ! Allocate vectors
+  m = 10
+  allocate(x(n*m), rhs(n), w(n), u(n), sigma(m), r(n))
+  allocate(res(m), itr(m))
+  rhs = (1.0d0, 0.0d0)
+  do i = 1,m
+     sigma(i) = 0.1d0 * exp( cmplx(0.0d0, 2*acos(-1.0d0)*(i-0.5d0) / real(m)) )
+  end do
+
+  ! Pre-process: Solve Bu = b
+  call zpptrf('U', n, B, info)
+  if (info /= 0) stop "zpptrf failed"
+  u = rhs
+  call zpptrs('U', n, 1, B, u, n, info)
+  if (info /= 0) stop "zpptrs failed"
+
+  ! Initialize solver
+  solver = gsminres_create(n, m);
+  call gsminres_initialize(solver, x, rhs, w, sigma, 1.0d-13, n, m)
+
+  ! Solving
+  do j = 1, 10000
+     call zhpmv('U', n, ONE, A, w, ZERO, u, 1)
+     call gsminres_glanczos_pre(solver, u, n)
+     u = w
+     call zpptrs('U', n, 1, B, u, 1, info)
+     if (info /= 0) stop "zpptrs failed"
+     call gsminres_glanczos_pst(solver, w, u, n)
+     if (gsminres_update(solver, x, n, m) /= 0) exit
+     call gsminres_get_residual(solver, res, m)
+  end do
+
+  ! Finalize solver
+  call gsminres_finalize(solver, itr, res, m)
+  call gsminres_destroy(solver)
+
+  ! Output Results
+  do i = 1, m
+     call zhpmv('U', n, (1.0d0,0.0d0), A, x((i-1)*n+1), (0.0d0,0.0d0), r, 1)
+     call zhpmv('U', n, sigma(i),      B, x((i-1)*n+1), (1.0d0,0.0d0), r, 1)
+     r = r - rhs
+     write(*, '(I2, 1X, 2F10.6, 1X, I5, 1X, 1P, E12.5, 1X, E12.5)') i, real(sigma), aimag(sigma), itr(i), res(i), dznrm2(n,r,1)
+  end do
+
+contains
+
+  subroutine load_matrix_from_mm(fname, A, n)
+    character(*), intent(in) ::fname
+    complex(c_double_complex), allocatable, intent(out) :: A(:)
+    integer(c_size_t), intent(out) :: n
+    integer :: i, j, k, row,col,nnz
+    complex(c_double_complex), allocatable :: full(:,:)
+    real(dp) :: re, im
+    character(len=256) :: line
+    logical :: sym
+    integer :: ios
+    open(unit=10, file=fname, status='old', action='read', iostat=ios)
+    if(ios /= 0) stop "Cannot open MTX file"
+    ! skip comments
+    do
+       read(10, '(A)', iostat=ios) line
+       if (ios /= 0) exit
+       if (line(1:1) /= '%') exit
+    end do
+    read(line,*) n, j, nnz
+    allocate(full(n,n)); full=(0.0d0, 0.0d0)
+    ! read matrix elements
+    do i = 1,nnz
+       read(10,*) row, col, re, im
+       full(row,col) = cmplx(re, im, kind=dp)
+       if (row /= col) full(col,row) = conjg(full(row,col))
+    end do
+    close(10)
+    ! convert packed upper triangular
+    allocate(A(n*(n+1)/2))
+    k = 0
+    do j = 1, n
+       do i = 1, j
+          k = k + 1
+          A(k) = full(i,j)
+       end do
+    end do
+  end subroutine load_matrix_from_mm
+
+end program sample2_f
---
+++ src/gsminres_fortran_interface.f90
@@ -0,0 +1,143 @@
+module gsminres_mod
+  use iso_c_binding, only: c_size_t, c_ptr, c_null_ptr, c_double, c_double_complex
+  implicit none
+
+  private
+
+  public :: gsminres_handle
+  public :: gsminres_create, gsminres_destroy
+  public :: gsminres_initialize
+  public :: gsminres_glanczos_pre, gsminres_glanczos_pst
+  public :: gsminres_update
+  public :: gsminres_finalize
+  public :: gsminres_get_residual
+
+  type, bind(C) :: gsminres_handle
+     type(c_ptr) :: ref
+  end type gsminres_handle
+
+contains
+
+  function gsminres_create(n, m) result(handle)
+    integer(c_size_t), value :: n, m
+    type(gsminres_handle) :: handle
+    interface
+       function c_gsminres_create(n, m) bind(C, name="gsminres_create")
+         import :: c_size_t, c_ptr
+         integer(c_size_t), value :: n, m
+         type(c_ptr) :: c_gsminres_create
+       end function c_gsminres_create
+    end interface
+    handle%ref = c_gsminres_create(n, m)
+  end function gsminres_create
+
+  subroutine gsminres_destroy(handle)
+    type(gsminres_handle), intent(inout) :: handle
+    interface
+       subroutine c_gsminres_destroy(h) bind(C, name="gsminres_destroy")
+         import :: c_ptr, c_null_ptr
+         type(c_ptr), value :: h
+       end subroutine c_gsminres_destroy
+    end interface
+    call c_gsminres_destroy(handle%ref)
+    handle%ref = c_null_ptr
+  end subroutine gsminres_destroy
+
+  subroutine gsminres_initialize(handle, x, b, w, sigma, threshold, n, m)
+    type(gsminres_handle),     intent(in) :: handle
+    complex(c_double_complex), intent(inout) :: x(*), w(*)
+    complex(c_double_complex), intent(in)     :: b(*), sigma(*)
+    real(c_double),            intent(in)     :: threshold
+    integer(c_size_t),         intent(in), value :: n, m
+    interface
+       subroutine c_gsminres_initialize(h, x, b, w, sigma, threshold, n, m) bind(C, name="gsminres_initialize")
+         import :: c_size_t, c_ptr, c_double, c_double_complex
+         type(c_ptr), value :: h
+         complex(c_double_complex) :: x(*), b(*), w(*), sigma(*)
+         real(c_double), value :: threshold
+         integer(c_size_t), value :: n, m
+       end subroutine c_gsminres_initialize
+    end interface
+    call c_gsminres_initialize(handle%ref, x, b, w, sigma, threshold, n, m)
+  end subroutine gsminres_initialize
+
+  subroutine gsminres_glanczos_pre(handle, u, n)
+    type(gsminres_handle), intent(in) :: handle
+    complex(c_double_complex), intent(inout) :: u(*)
+    integer(c_size_t), intent(in), value :: n
+    interface
+       subroutine c_gsminres_glanczos_pre(h, u, n) bind(C, name="gsminres_glanczos_pre")
+         import :: c_size_t, c_ptr, c_double_complex
+         type(c_ptr), value :: h
+         complex(c_double_complex) :: u(*)
+         integer(c_size_t), value :: n
+       end subroutine c_gsminres_glanczos_pre
+    end interface
+    call c_gsminres_glanczos_pre(handle%ref, u, n)
+  end subroutine gsminres_glanczos_pre
+
+  subroutine gsminres_glanczos_pst(handle, w, u, n)
+    type(gsminres_handle), intent(in) :: handle
+    complex(c_double_complex), intent(inout) :: w(*), u(*)
+    integer(c_size_t), intent(in), value :: n
+    interface
+       subroutine c_gsminres_glanczos_pst(h, w, u, n) bind(C, name="gsminres_glanczos_pst")
+         import :: c_size_t, c_ptr, c_double_complex
+         type(c_ptr), value :: h
+         complex(c_double_complex) :: w(*), u(*)
+         integer(c_size_t), value :: n
+       end subroutine c_gsminres_glanczos_pst
+    end interface
+    call c_gsminres_glanczos_pst(handle%ref, w, u, n)
+  end subroutine gsminres_glanczos_pst
+
+  function gsminres_update(handle, x, n, m) result(flag)
+    type(gsminres_handle), intent(in) :: handle
+    complex(c_double_complex), intent(inout) :: x(*)
+    integer(c_size_t), intent(in), value :: n, m
+    integer :: flag
+    interface
+       function c_gsminres_update(h, x, n, m) bind(C, name="gsminres_update")
+         import :: c_size_t, c_ptr, c_double_complex
+         type(c_ptr), value :: h
+         complex(c_double_complex) :: x(*)
+         integer(c_size_t), value :: n, m
+         integer :: c_gsminres_update
+       end function c_gsminres_update
+    end interface
+    flag = c_gsminres_update(handle%ref, x, n ,m)
+  end function gsminres_update
+
+  subroutine gsminres_finalize(handle, conv_itr, conv_res, m)
+    type(gsminres_handle), intent(in) :: handle
+    integer, intent(out) :: conv_itr(*)
+    real(c_double), intent(out) :: conv_res(*)
+    integer(c_size_t), intent(in), value :: m
+    interface
+       subroutine c_gsminres_finalize(h, conv_itr, conv_res, m) bind(C, name="gsminres_finalize")
+         import :: c_size_t, c_ptr, c_double
+         type(c_ptr), value :: h
+         integer :: conv_itr(*)
+         real(c_double) :: conv_res(*)
+         integer(c_size_t), value :: m
+       end subroutine c_gsminres_finalize
+    end interface
+    call c_gsminres_finalize(handle%ref, conv_itr, conv_res, m)
+  end subroutine gsminres_finalize
+
+  subroutine gsminres_get_residual(handle, res, m)
+    type(gsminres_handle), intent(in) :: handle
+    real(c_double), intent(out) :: res(m)
+    integer(c_size_t), intent(in), value :: m
+    interface
+       subroutine c_gsminres_get_residual(h, res, m) bind(C, name="gsminres_get_residual")
+         import :: c_size_t, c_ptr, c_double
+         type(c_ptr), value :: h
+         real(c_double) :: res(*)
+         integer(c_size_t), value :: m
+       end subroutine c_gsminres_get_residual
+    end interface
+    call c_gsminres_get_residual(handle%ref, res, m)
+  end subroutine gsminres_get_residual
+
+end module gsminres_mod
