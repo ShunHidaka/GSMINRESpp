6167878d20e65dc8b000b4ca862ca9ada4bfc71a
hidaka@HOME <stern.komo@gmail.com>
2025-05-13 00:34:56 +0900

    M_PI が定義されていないとなったので手動で定義

--- sample/sample2_c.c
+++ sample/sample2_c.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 #include <complex.h>
 
 extern void zcopy_(const int*, const double _Complex*, const int*, double _Complex*, const int*);
